9701521f32149e8a7ce74c6f0d1070ee8b3acd45
GennadyMukchamedchanov <dungreshmen2@mail.ru>
2025-03-31 21:38:51 +0300

    start

---
+++ user_auth/conf/settings.py
@@ -0,0 +1,117 @@
+
+from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env.read_env(os.path.join(BASE_DIR, "user_auth/.env"))
+
+
+SECRET_KEY = env("SECRET_KEY", cast=str)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG", cast=str)
+
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'user.apps.UserConfig',
+    'authentication.apps.AuthenticationConfig',
+    'db.apps.DbConfig'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'conf.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'conf.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+     'default': env.db()
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+
+STATIC_URL = env("STATIC_URL", cast=str)
+STATIC_ROOT = os.path.join(BASE_DIR, env("STATIC_ROOT", cast=str, default="static/"))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
+MEDIA_URL = env("MEDIA_URL", cast=str)
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'db.User'
---
+++ user_auth/db/models/user.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+from .manager import CustomUserManager
+
+
+class User(AbstractUser):
+    phone_validator = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Номер телефона должен быть в формате: '+999999999'. Должно быть от 9 до 15 цифр."
+    )
+    phone_number = models.CharField(max_length=15, validators=[phone_validator],
+                                    blank=False, null=False, unique=True, verbose_name='Телефон')
+    username = models.CharField(max_length=150, verbose_name='Имя')
+    is_manager = models.BooleanField(default=False, verbose_name='Является менеджером')
+    is_admin = models.BooleanField(default=False, verbose_name='Является админом')
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username', 'email']
+    objects = CustomUserManager()
+
+
+
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        app_label = 'db'
+
+    def __str__(self):
+        return self.username  # Возвращает имя пользователя при вызове str()
+
